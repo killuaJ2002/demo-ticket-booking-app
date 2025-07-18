053c81dd82dddcb72c9571dde3bb73380207de25
killuaJ2002 <kjcodes2002@gmail.com>
2025-07-18 14:56:26 +0530

    changed script to work with backend

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 const form = document.querySelector(".form__section");
 
-const handleSubmit = (e) => {
+const handleSubmit = async (e) => {
   e.preventDefault(); // prevent page reload
 
   const from = document.getElementById("fromInput").value;
   const to = document.getElementById("toInput").value;
   const date = document.getElementById("dateInput").value;
 
-  const newEntry = { from, to, date };
+  try {
+    const response = await fetch("http://localhost:8000/api/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        from,
+        to,
+        date,
+      }),
+    });
+    if (!response.ok) throw new Error("Couldn't add new data");
+    const result = await response.json();
+    console.log(result.data);
+  } catch (error) {
+    console.log(error);
+  }
 
-  // Step 1: Read existing data
-  let existing = JSON.parse(localStorage.getItem("data")) || [];
-
-  // Step 2: Add new entry
-  existing.push(newEntry);
-
-  // Step 3: Save updated array
-  localStorage.setItem("data", JSON.stringify(existing));
-  console.log(JSON.parse(localStorage.getItem("data")));
+  document.getElementById("fromInput").value = "";
+  document.getElementById("toInput").value = "";
+  document.getElementById("dateInput").value = "";
 };
 
 form.addEventListener("submit", handleSubmit);
